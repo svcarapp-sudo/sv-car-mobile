ae289bc4ac7717195b8ca40be0fbc7e0f0ecb63c
svcarapp-sudo <svcarapp-sudo@users.noreply.github.com>
2025-12-27 21:18:03 +0300

    Add babel module resolver for path aliases

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@/components': './src/components',
+            '@/screens': './src/screens',
+            '@/navigation': './src/navigation',
+            '@/services': './src/services',
+            '@/utils': './src/utils',
+            '@/types': './src/types',
+            '@/constants': './src/constants',
+            '@/hooks': './src/hooks',
+            '@/store': './src/store',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        },
+      ],
+    ],
+  };
+};
+
